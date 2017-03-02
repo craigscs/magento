aaa0cc2a4ce9cfc9bd0ebae6795e7afdd3c051c6
Craig Standish <craigscs@gradusgroup.com>
2017-03-02 11:13:12 -0500

    Image uploading fixed.

--- shell/import/images.php
+++ shell/import/images.php
@@ -10,11 +10,24 @@ $state = $obj->get('Magento\Framework\App\State');
 $state->setAreaCode('adminhtml');
 
 $pr = $obj->create('Magento\Catalog\Model\ProductRepository');
-$file = fopen('images.csv', 'r');
+$process = $obj->create('Magento\Catalog\Model\Product\Gallery\Processor');
+$file = fopen('shell/import/images.csv', 'r');
 $c = 0;
-while (($row = fgetcsv($file)) !== FALSE) {
-    if ($c ==0) {
+$cc = 0;
+
+$file2 = fopen('shell/import/csv/links.csv', 'r');
+$links = array();
+while (($row = fgetcsv($file2, 4096)) !== false) {
+    if ($c == 0) {
         $c++;
+        continue;
+    }
+    $links[$row[3]] = $row[2];
+}
+
+while (($row = fgetcsv($file)) !== FALSE) {
+    if ($cc ==0) {
+        $cc++;
         continue;
     }
     if(!isset($productData[$row[1]]))
@@ -30,13 +43,24 @@ while (($row = fgetcsv($file)) !== FALSE) {
 }
 fclose($file);
 foreach ($productData as $sku => $value) {
-    $p = $pr->get($sku);
-    $img = file_get_contents($value[1]['file']);
-    $im = imagecreatefromstring($img);
-    imagepng($im, "image/".$value[1]['name']);
-    $imagePath = "http://gradus.dev/shell/import/".$value[1]['name']; // path of the image])
-    var_dump($imagePath);
-    $p->addImageToMediaGallery($imagePath, array('image', 'small_image', 'thumbnail'), false, false);
-    //    $p->setData('features', json_encode($value));
-//    $p->getResource()->saveAttribute($p, 'features');
+
+    try {
+        if (isset($links[$sku])) {
+            $sku = $links[$sku];
+        }
+        $p = $pr->get($sku);
+        $url = $value[1]['file'];
+        $img = 'pub/media/bhimp/' . $value[1]['name'];
+        file_put_contents($img, file_get_contents($url));
+        $p->addImageToMediaGallery('/bhimp/' . $value[1]['name'], array('image', 'small_image', 'thumbnail'), false, false);
+
+        $gallery = $p->getData('media_gallery');
+        $lastimg = array_pop($gallery['images']);
+        $process->updateImage($p, $lastimg['file'], array('label' => $value[1]['caption'], 'label_default' => $value[1]['caption'], 'position' => $value[1]['pos']));
+        $p->save();
+        var_dump($p->getMediaGallery());
+        printf("SKU saved: ".$sku);
+    } catch (\Exception $e) {
+        printf($e->getMessage()." :: ".$sku);
+    }
 }
