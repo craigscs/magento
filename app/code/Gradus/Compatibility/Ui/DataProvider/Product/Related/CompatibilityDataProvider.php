1a73cff30acbb8d2a93edcf920ed4dd214c51899
Craig Standish <craigscs@gradusgroup.com>
2017-03-08 16:42:03 -0500

    No prefiltering on compat selection

--- app/code/Gradus/Compatibility/Ui/DataProvider/Product/Related/CompatibilityDataProvider.php
+++ app/code/Gradus/Compatibility/Ui/DataProvider/Product/Related/CompatibilityDataProvider.php
@@ -3,43 +3,12 @@
 namespace Gradus\Compatibility\Ui\DataProvider\Product\Related;
 
 use Magento\Catalog\Ui\DataProvider\Product\Related\AbstractDataProvider;
-use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 
 /**
  * Class CustomTypeDataProvider
  */
-class CompatibilityDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+class CompatibilityDataProvider extends AbstractDataProvider
 {
-
-    /**
-     * Construct
-     *
-     * @param string $name
-     * @param string $primaryFieldName
-     * @param string $requestFieldName
-     * @param CollectionFactory $collectionFactory
-     * @param \Magento\Ui\DataProvider\AddFieldToCollectionInterface[] $addFieldStrategies
-     * @param \Magento\Ui\DataProvider\AddFilterToCollectionInterface[] $addFilterStrategies
-     * @param array $meta
-     * @param array $data
-     */
-    public function __construct(
-        $name,
-        $primaryFieldName,
-        $requestFieldName,
-        CollectionFactory $collectionFactory,
-        array $addFieldStrategies = [],
-        array $addFilterStrategies = [],
-        array $meta = [],
-        array $data = []
-    ) {
-        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
-        $this->collection = $collectionFactory->create();
-        $this->collection->addAttributeToFilter('gradus_type', 6);
-        $this->addFieldStrategies = $addFieldStrategies;
-        $this->addFilterStrategies = $addFilterStrategies;
-    }
-
     /**
      * {@inheritdoc
      */
