a71eeedfbd7ac8cbf36d77b7ebd9aeca81caab1e
Craig Standish <craigscs@gradusgroup.com>
2017-03-30 17:25:55 -0400

    put call in phtml files instead

--- app/code/Gradus/Catalog/view/adminhtml/requirejs-config.js
+++ app/code/Gradus/Catalog/view/adminhtml/requirejs-config.js
@@ -1,10 +1,10 @@
 var config = {
     map: {
         '*': {
-            chosen: 'Gradus_Catalog/js/chosen.jquery',
+            chosen: 'Gradus_Requires/js/chosen.jquery',
         }
     },
     paths: {
-        'chosen': 'Gradus_Catalog/js/chosen.jquery',
+        'chosen': 'Gradus_Requires/js/chosen.jquery',
     },
 };
