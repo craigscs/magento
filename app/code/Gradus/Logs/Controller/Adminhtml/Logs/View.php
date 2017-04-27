c9b60ceb40703a1abcd287832d46b6415d9bbad4
Craig Standish <craigscs@gradusgroup.com>
2017-04-27 14:19:15 -0400

    missed files

--- app/code/Gradus/Logs/Controller/Adminhtml/Logs/View.php
+++ app/code/Gradus/Logs/Controller/Adminhtml/Logs/View.php
@@ -6,28 +6,22 @@
  * Time: 11:43 AM
  */
 
-namespace Gradus\Importer\Controller\Adminhtml\Import;
+namespace Gradus\Logs\Controller\Adminhtml\Logs;
 
 use Magento\Framework\App\ResponseInterface;
 
 class View extends \Magento\Backend\App\Action
 {
-    protected $imp;
-
     public function __construct(\Magento\Backend\App\Action\Context $context)
     {
-        parent::__construct($context);
-        $this->imp = $this->_objectManager->create('\Gradus\Importer\Model\Imports');
+        parent::__construct($context);;
     }
 
     public function execute()
     {
-        $id = $this->getRequest()->getParam('import_id');
-        $imp = $this->imp->load($id);
         $this->_view->loadLayout();
         $this->_view->getLayout()->initMessages();
-        $block = $this->_view->getLayout()->getBlock('importer.import.view');
-        $block->setImport($imp);
+        $block = $this->_view->getLayout()->getBlock('logs.logs.view');
         $this->_view->renderLayout();
     }
 }
