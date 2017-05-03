2be79d759a56388c5b4e2265c993a3e5c425ac39
Craig Standish <craigscs@gradusgroup.com>
2017-05-03 13:47:05 -0400

    Grid to xml will now work

---
+++ app/code/Gradus/Catalog/Ui/GridToXml.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Gradus\Catalog\Ui;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Response\Http\FileFactory;
+
+/**
+ * Class Render
+ */
+class GridToXml extends Action
+{
+    /**
+     * @var ConvertToXml
+     */
+    protected $converter;
+
+    /**
+     * @var FileFactory
+     */
+    protected $fileFactory;
+
+    /**
+     * @param Context $context
+     * @param ConvertToXml $converter
+     * @param FileFactory $fileFactory
+     */
+    public function __construct(
+        Context $context,
+        ConvertToXml $converter,
+        FileFactory $fileFactory
+    ) {
+        parent::__construct($context);
+        $this->converter = $converter;
+        $this->fileFactory = $fileFactory;
+    }
+
+    /**
+     * Export data provider to XML
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return \Magento\Framework\App\ResponseInterface
+     */
+    public function execute()
+    {
+        return $this->fileFactory->create('export.xml', $this->converter->getXmlFile(), 'var');
+    }
+}
