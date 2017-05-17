416b4d4d0503746d60d90115e1b82637ace6f71d
Craig Standish <craigscs@gradusgroup.com>
2017-05-17 14:18:09 -0400

    sticky header added to table

--- app/code/Gradus/Importer/view/adminhtml/requirejs-config.js
+++ app/code/Gradus/Importer/view/adminhtml/requirejs-config.js
@@ -1,10 +1,12 @@
 var config = {
     map: {
         '*': {
-            datatables: 'Gradus_Importer/js/datatables'
+            datatables: 'Gradus_Importer/js/datatables',
+            sticky: 'Gradus_Importer/js/jquery.sticky-kit.min'
         }
     },
     paths: {
-        'datatables': 'Gradus_Importer/js/datatables'
+        'datatables': 'Gradus_Importer/js/datatables',
+        'sticky': 'Gradus_Importer/js/jquery.sticky-kit.min'
     },
 };
--- app/code/Gradus/Importer/view/adminhtml/web/js/datatable.js
+++ app/code/Gradus/Importer/view/adminhtml/web/js/datatable.js
@@ -1,8 +1,9 @@
-require(['jquery','datatables'],function($){
+require(['jquery','datatables', 'sticky'],function($){
     $(window).load(function() {
         $('#messages').dataTable({
             paging: true,
             "dom": "lifrtp"
         });
+        $('#tr_stick').stick_in_parent();
     })
 });
