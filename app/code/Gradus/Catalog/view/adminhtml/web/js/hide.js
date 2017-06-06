bf46940947f838d9ad54b0f2aabe2ad16a2b23a8
Craig Standish <craigscs@gradusgroup.com>
2017-06-06 10:52:03 -0400

    this now hides the fields properly

--- app/code/Gradus/Catalog/view/adminhtml/web/js/hide.js
+++ app/code/Gradus/Catalog/view/adminhtml/web/js/hide.js
@@ -1,8 +1,8 @@
 require(['jquery'],function(q) {
-    q( document ).ready(function() {
+    q( document ).ajaxComplete(function() {
         q('[name="product[in_the_box]"]').parent().parent().hide();
         q('[name="product[features]"]').parent().parent().hide();
-        q('[name="product[techspecs]"]').parent().parent().hide();
+        q('[name="product[tech_specs]"]').parent().parent().hide();
         q('[name="product[highlights]"]').parent().parent().hide();
     })
 });
